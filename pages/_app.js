41be720b4c958e00f4796a1bb31859fdd9ec6a15
Pawan Kumar <surpawan@gmail.com>
2022-03-18 22:56:41 +0530

    Added changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
