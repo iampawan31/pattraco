ce09e800013aa0d2a805988b686f8f5ae6167442
Pawan Kumar <surpawan@gmail.com>
2022-03-25 15:11:54 +0530

    Added Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,12 @@
-import { useEffect, useState, useRef } from 'react'
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import useVH from 'react-viewport-height'
 
 function MyApp({ Component, pageProps }) {
-  const [backgroundImage, setBackgroundImage] = useState('')
-  const bgImageRef = useRef()
-
-  useEffect(() => {
-    function resize() {
-      console.log('called')
-      // We execute the same script as before
-      let vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
-
-    window.addEventListener('resize', resize)
-    window.addEventListener('load', resize)
-
-    return () => {
-      window.removeEventListener('resize', resize)
-      window.removeEventListener('load', resize)
-    }
-  }, [])
-
+  useVH()
   return (
-    <Layout bgImageRef={bgImageRef} bgImage={backgroundImage}>
-      <Component setBackgroundImage={setBackgroundImage} {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
     </Layout>
   )
 }
