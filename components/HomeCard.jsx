9a3f121c02628832104d6f64a12ecc660ae50eb9
Pawan Kumar <surpawan@gmail.com>
2022-03-28 00:34:47 +0530

    Added Changes

--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -4,17 +4,23 @@ import Image from 'next/image'
 // eslint-disable-next-line react/display-name
 const HomeCard = forwardRef(({ href, imageSrc, mainTitle, subTitle }, ref) => {
   return (
-    <a href={href} className="group rounded">
-      <div className="relative h-48 md:h-full w-full shadow rounded">
+    <a href={href} className="group">
+      <div className="relative h-56 sm:h-60 md:h-full w-full">
         <Image
           src={imageSrc}
           layout="fill"
           objectFit="cover"
-          className="rounded shadow group-hover:scale-105 transform group-hover:rounded transition duration-300"
+          className="group-hover:scale-105 transform transition duration-300"
           alt={mainTitle}
         />
-        <div className="rounded bg-gradient-to-t from-black/90 to-black/30 group-hover:from-black/80 transition duration-300 group-hover:to-black/60 w-full h-full py-2 text-left content-end cursor-pointer absolute top-0 left-0 flex flex-col justify-end text-gray-300 z-30 font-extralight px-2">
-          <div className="text-4xl font-normal">{mainTitle}</div>
+        <div
+          className="bg-gradient-to-t from-black/90 to-black/10
+        group-hover:from-black/90 transition duration-300
+        group-hover:to-black/30 w-full h-full py-6 text-left
+        content-end cursor-pointer absolute top-0 left-0 flex
+        flex-col justify-end text-gray-300 z-30 font-extralight px-2"
+        >
+          <div className="text-3xl font-normal">{mainTitle}</div>
           <div className="">{subTitle}</div>
         </div>
       </div>
