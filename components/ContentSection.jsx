e5ea4a3999118cc07b3c23b047d75d81a0ccc855
Pawan Kumar <surpawan@gmail.com>
2022-03-30 23:00:24 +0530

    Added Further Changes

--- components/ContentSection.jsx
+++ components/ContentSection.jsx
@@ -7,10 +7,10 @@ const ContentSection = ({ mainTitle, content }) => {
       <div className="border-b border-gray-800">
         <div className="container mx-auto py-6 md:py-16 px-2 md:px-0 w-full">
           <div className="flex flex-col md:flex-row w-full text-gray-800">
-            <div className="text-3xl font-semibold md:text-5xl md:font-light md:w-2/6 pb-4 md:pb-0">
+            <div className="text-3xl font-semibold md:text-5xl md:font-light md:w-2/6 pb-4 md:pb-0 md:px-2">
               {mainTitle}
             </div>
-            <div className="text-xl md:text-2xl md:w-4/6 font-light">
+            <div className="text-xl md:text-2xl md:w-4/6 font-light md:px-10">
               {content}
             </div>
           </div>
