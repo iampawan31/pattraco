d64c1cbe6ae21d9f5dc809ee215915adbf70971e
Pawan Kumar <surpawan@gmail.com>
2022-04-12 13:52:06 +0530

    Added Layout Changes

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ const layout = ({ children }) => {
         <meta name="description" content="Pattraco Website" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="w-full scroll-smooth font-primary text-secondary-font font-light h-full min-h-screen">
+      <div className="w-full scroll-smooth font-primary text-secondary-font font-normal h-full min-h-screen">
         <Navbar />
         <div className="w-full">{children}</div>
         <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         primary: '#97B434',
