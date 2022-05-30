8c424be1978ee6f63df37be2e26e82d52fb2d6a2
Pawan Kumar <surpawan@gmail.com>
2022-05-30 15:40:37 +0530

    Added Google Analytics

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -10,6 +11,20 @@ const layout = ({ children }) => {
         <meta name="description" content="Pattraco Website" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics-tag" strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <div className="w-full scroll-smooth font-primary text-secondary-font font-normal h-full min-h-screen">
         <Navbar />
         <div className="w-full">{children}</div>
