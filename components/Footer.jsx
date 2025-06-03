830898c05278abe8bcd59852223c07d968c0438e
Pawan Kumar <surpawan@gmail.com>
2025-06-03 16:27:34 +0530

    fix: footer link not styling correctly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
 
   return (
     <div className="w-full h-full lg:h-16 space-y-6 bg-primary lg:space-y-1 flex flex-col justify-center items-center uppercase py-10">
-      <div className="flex">
+      <div className="flex flex-col md:flex-row items-center md: gap-2">
         <div className="">
           <Link href="/privacy-policy">
-            <a className="hover:border-b-4 hover:border-primary transition h-8 text-xl lg:text-base pb-2">
+            <a className="hover:border-b-4 hover:border-primary transition h-8 text-base pb-2">
               Privacy Policy
             </a>
           </Link>
